bf935e4190c43cf2c2ac131bf9ff0ed52a0c6fd8
Nicklas <136376191+koljaPl@users.noreply.github.com>
2026-08-30 09:59:59 +0200

    Add: count-paths - submission-1

---
+++ Data Structures & Algorithms/count-paths/submission-1.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        vector<int> row(n, 1);
+
+        for (int i = 0; i < m - 1; i++) {
+            vector<int> newRow(n, 1);
+
+            for (int j = n - 2; j > -1; j--) {
+                newRow[j] = newRow[j + 1] + row[j];
+            }
+
+            row = newRow;
+        }
+
+        return row[0];
+    }
+};
