d7c0e728895162e0284c55dafcd0176e148e5bfb
Nicklas <136376191+koljaPl@users.noreply.github.com>
2026-09-04 16:45:47 +0200

    Add: set-zeroes-in-matrix - submission-6

---
+++ Data Structures & Algorithms/set-zeroes-in-matrix/submission-6.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        int m = matrix[0].size();
+        
+        bool first_row_zero = false;
+        for (int j = 0; j < m; ++j) {
+            if (matrix[0][j] == 0) {
+                first_row_zero = true;
+                break;
+            }
+        }
+
+        bool first_col_zero = false;
+        for (int i = 0; i < n; ++i) {
+            if (matrix[i][0] == 0) {
+                first_col_zero = true;
+                break;
+            }
+        }
+
+        for (int i = 1; i < n; ++i) {
+            for (int j = 1; j < m; ++j) {
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+
+        for (int i = 1; i < n; ++i) {
+            for (int j = 1; j < m; ++j) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        if (first_row_zero) {
+            for (int j = 0; j < m; ++j) {
+                matrix[0][j] = 0;
+            }
+        }
+
+        if (first_col_zero) {
+            for (int i = 0; i < n; ++i) {
+                matrix[i][0] = 0;
+            }
+        }
+    }
+};
